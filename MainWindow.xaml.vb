b5d0340da90b83c3d7c0460fe0510f28eb609473
WJPavocristatus <wjpeacoc@iu.edu>
2025-09-18 13:47:08 -0400

    timers and color changes are working!

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Diagnostics
+Imports System.Reflection.Emit
 Imports System.Windows
 Imports System.Windows.Media
 Imports Phidget22
@@ -7,50 +8,79 @@ Imports Phidget22.Events
 Public Class MainWindow
     'Inherits Window
 
-    Private bc As DigitalInput
-    'Public bg As Controls.Grid.Name.Equals("StimGrid")
+    Private bc As DigitalInput = New DigitalInput()
+    Friend WithEvents timer As New System.Timers.Timer
     Public btnCount As Integer = 0
 
-    Public Property pressTimer As Stopwatch = New Stopwatch()
-    Public Property activeStimulus As Stopwatch = New Stopwatch()
-    Public Property stimATimer As Stopwatch = New Stopwatch()
-    Public Property stimBTimer As Stopwatch = New Stopwatch()
+    Public Property PressWatch As Long
+    Public Property ActiveStimWatch As Stopwatch = New Stopwatch()
+    Public Property StimAWatch As Stopwatch = New Stopwatch()
+    Public Property StimBWatch As Stopwatch = New Stopwatch()
 
-    Private Sub Initiate() Handles MyBase.Loaded
-
-        'Me.DataContext = Me
-
-        bc = New DigitalInput()
-
-
+    Public Sub New()
         bc.DeviceSerialNumber = 705800
         bc.Channel = 13
-
-
+        timer.Start()
+        timer.Interval = 1
+        Log.Enable(LogLevel.Info, "file.log")
 
         AddHandler bc.Attach, AddressOf OnAttachHandler
         AddHandler bc.StateChange, AddressOf BCh_StateChange
 
         bc.Open()
+        Clock()
     End Sub
 
+    Private Sub Clock() Handles timer.Elapsed
+        Application.Current.Dispatcher.BeginInvoke(New Action(AddressOf controlloop))
+    End Sub
+
+    Private Sub controlloop()
+        ActiveStimVal.Content = ActiveStimWatch.ElapsedMilliseconds
+        PressWatchVal.Content = StimAWatch.ElapsedMilliseconds + StimBWatch.ElapsedMilliseconds
+        StimAWatchVal.Content = StimAWatch.ElapsedMilliseconds
+        StimBWatchVal.Content = StimBWatch.ElapsedMilliseconds
+        If (StimAWatch.ElapsedMilliseconds + StimBWatch.ElapsedMilliseconds >= 1000) Then
+            ResetTrial()
+        End If
+        If (ActiveStimWatch.ElapsedMilliseconds >= 100) Then
+            StimGrid.Background = Brushes.Black
+            ActiveStimWatch.Stop()
+            StimAWatch.Stop()
+            StimBWatch.Stop()
+        End If
+    End Sub
+
+    Private Sub SetGridColor(count As Integer)
+        Select Case (count Mod 2)
+            Case 0
+                StimGrid.Background = Brushes.Blue
+                ActiveStimWatch.Start()
+                StimAWatch.Start()
+            Case 1
+                StimGrid.Background = Brushes.Orange
+                ActiveStimWatch.Start()
+                StimBWatch.Start()
+        End Select
+    End Sub
     Private Sub BCh_StateChange(sender As Object, e As DigitalInputStateChangeEventArgs)
         Dispatcher.Invoke(Sub()
                               If e.State Then
-                                  btnCount += 1
 
-                                  Select Case (btnCount Mod 2)
-                                      Case 0
-                                          StimGrid.Background = Brushes.Blue
-                                          stimATimer.Start()
-                                      Case 1
-                                          StimGrid.Background = Brushes.Orange
-                                          stimBTimer.Start()
-                                  End Select
+                                  If (ActiveStimWatch.ElapsedMilliseconds <= 10000) Then
+                                      btnCount = btnCount + 1
+                                      ActiveStimWatch.Reset()
+                                      SetGridColor(btnCount)
+                                  ElseIf (ActiveStimWatch.ElapsedMilliseconds > 10000) Then
+                                      ActiveStimWatch.Reset()
+                                      StimAWatch.Reset()
+                                      StimBWatch.Reset()
+
+                                  End If
                               Else
-                                  activeStimulus.Stop()
-                                  stimATimer.Stop()
-                                  stimBTimer.Stop()
+                                  ActiveStimWatch.Stop()
+                                  StimAWatch.Stop()
+                                  StimBWatch.Stop()
                                   StimGrid.Background = Brushes.Black
                               End If
                           End Sub)
@@ -58,13 +88,22 @@ Public Class MainWindow
 
     Private Sub OnAttachHandler(sender As Object, e As AttachEventArgs)
         Dispatcher.Invoke(Sub()
-                              Initiate()
+                              Log.WriteLine(LogLevel.Info, "Phidget button attached!")
                           End Sub)
+
     End Sub
 
     ' Reset Stopwatches to 0 for new trial after pressTimer reaches 1 minute
     Private Sub ResetTrial()
-        pressTimer.Reset()
+        btnCount = 0
+        PressWatch = 0
+        StimGrid.Background = Brushes.Black
+        ActiveStimWatch.Stop()
+        StimAWatch.Stop()
+        StimBWatch.Stop()
+        ActiveStimWatch.Reset()
+        StimAWatch.Reset()
+        StimBWatch.Reset()
     End Sub
 
     ' Clean up the Phidget resources when the application closes
