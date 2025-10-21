6f252d60f9e127624dbc379eda293f22e0bc93e7
Jake Peacock <wjpeacoc@iu.edu>
2025-10-21 08:58:33 -0400

    push from laptop to dev that implements two windows on startup

--- Application.xaml.vb
+++ Application.xaml.vb
@@ -5,8 +5,6 @@
 
     Private Sub App_Startup(sender As Object, e As StartupEventArgs)
         Dim cw As New ChimpWindow()
-        cw.Left = 400 ' Set position
-        cw.Top = 200
         cw.Show() ' Show the second window
     End Sub
 End Class
--- ChimpWindow.xaml.vb
+++ ChimpWindow.xaml.vb
@@ -6,7 +6,8 @@ Imports Phidget22
 Imports Phidget22.Events
 
 Public Class ChimpWindow
-    Private ph As Phidget
+    Dim opMode As String
+    Private ph As New DigitalInput()
     'Private pc As New DigitalOutput() '<-- INTENDED FOR PHYSICAL (I.E., LED) PROGRESS BA; pc = "Progress Channel"
     Private bc As New DigitalInput() 'bc = "Button Channel"
     'Private fc As New DigitalOutput() 'fc = "Feeder Channel"
@@ -23,17 +24,24 @@ Public Class ChimpWindow
     Public Sub New()
 
         ' This call is required by the designer.
-        'InitializeComponent()
+        InitializeComponent()
 
         ' Add any initialization after the InitializeComponent() call.
 
-
-
+        ph.DeviceSerialNumber = 705800
+        ph.Channel = 0
+        Try
+            ph.Open(3000)
+            opMode = "attached"
+        Catch ex As PhidgetException
+            MessageBox.Show($"{ex.Message} Entering virtual testing mode!")
+            opMode = "virtual"
+        End Try
 
 
         bc.DeviceSerialNumber = 705800
         'fc.DeviceSerialNumber = 705800
-        bc.Channel = 0
+        bc.Channel = 1
         'fc.Channel = 15
         'pc.DeviceSerialNumber = 705800
         'pc.Channel = 2
@@ -43,15 +51,19 @@ Public Class ChimpWindow
 
         AddHandler bc.Attach, AddressOf OnAttachHandler
         'AddHandler fc.Attach, AddressOf OnAttachHandler
-
-        AddHandler bc.StateChange, AddressOf BCh_StateChange
+        DetectMode()
+        'AddHandler bc.StateChange, AddressOf BCh_StateChange
         'AddHandler pc.Attach, AddressOf OnAttachHandler
+
 
         'If (pc.Attached) Then
         '    pc.Open()
         '    pc.State = False
         'End If
-        bc.Open()
+        If (bc.Attached) Then
+            bc.Open(5000)
+        End If
+
         'If (fc.Attached) Then
         'fc.Open()
         'End If
@@ -59,32 +71,41 @@ Public Class ChimpWindow
     End Sub
 
     Private Sub Clock() Handles timer.Elapsed
-            Application.Current.Dispatcher.BeginInvoke(New Action(AddressOf controlloop))
-        End Sub
+        Application.Current.Dispatcher.BeginInvoke(New Action(AddressOf ControlLoop))
+    End Sub
 
-        Private Sub controlloop()
+    Private Sub DetectMode()
+
+        If opMode Is "attached" Then
+            VirtualBtn.Visibility = Visibility.Hidden
+        ElseIf opMode Is "virtual" Then
+            VirtualBtn.Visibility = Visibility.Visible
+        End If
+    End Sub
+
+    Private Sub ControlLoop()
         'InitWatches() 'sets values in UI
 
         If (StimAWatch.ElapsedMilliseconds + StimBWatch.ElapsedMilliseconds >= 10000) Then 'check that button holding time isn't over 100 seconds
-                'fc.State = True 'activate feeder for banana pellet if target time met
-                LockOut()
-                ResetTrial() 'reset the trial values
-            End If
+            'fc.State = True 'activate feeder for banana pellet if target time met
+            LockOut()
+            ResetTrial() 'reset the trial values
+        End If
 
-            If (ActiveStimWatch.ElapsedMilliseconds >= 1000) Then
-                StimGrid.Background = Brushes.Black
-                ActiveStimWatch.Stop()
-                StimAWatch.Stop()
-                StimBWatch.Stop()
-            End If
+        If (ActiveStimWatch.ElapsedMilliseconds >= 1000) Then
+            StimGrid.Background = Brushes.Black
+            ActiveStimWatch.Stop()
+            StimAWatch.Stop()
+            StimBWatch.Stop()
+        End If
 
-            'Dim prog = PressWatch Mod 10000
-            'If (prog.Equals(0)) Then
-            '    pc.State = True
-            'ElseIf (prog > 0) Then
-            '    pc.State = False
-            'End If
-        End Sub
+        'Dim prog = PressWatch Mod 10000
+        'If (prog.Equals(0)) Then
+        '    pc.State = True
+        'ElseIf (prog > 0) Then
+        '    pc.State = False
+        'End If
+    End Sub
 
     'Private Sub InitWatches()
     '    ActiveStimVal.Content = ActiveStimWatch.ElapsedMilliseconds
@@ -94,69 +115,93 @@ Public Class ChimpWindow
     'End Sub
 
     Private Sub SetGridColor(count As Integer)
-            Select Case (count Mod 2)
-                Case 0
-                    ActiveStimWatch.Start()
-                    StimAWatch.Start()
-                    StimGrid.Background = Brushes.White
-                Case 1
-                    ActiveStimWatch.Start()
-                    StimBWatch.Start()
-                    StimGrid.Background = Brushes.Red
-            End Select
-        End Sub
+        Select Case (count Mod 2)
+            Case 0
+                ActiveStimWatch.Start()
+                StimAWatch.Start()
+                StimGrid.Background = Brushes.White
+            Case 1
+                ActiveStimWatch.Start()
+                StimBWatch.Start()
+                StimGrid.Background = Brushes.Red
+        End Select
+    End Sub
 
-        Private Sub BCh_StateChange(sender As Object, e As DigitalInputStateChangeEventArgs)
-            Dispatcher.Invoke(Sub()
-                                  If e.State Then
-                                      Latency.Stop()
-                                      If (ActiveStimWatch.ElapsedMilliseconds <= 10000) Then
-                                          btnCount = btnCount + 1
-                                          ActiveStimWatch.Reset()
-                                          SetGridColor(btnCount)
-                                      ElseIf (ActiveStimWatch.ElapsedMilliseconds >= 10000) Then
-                                          ActiveStimWatch.Reset()
-                                          StimAWatch.Reset()
-                                          StimBWatch.Reset()
-                                      End If
-                                  Else
-                                      Latency.Start()
-                                      ActiveStimWatch.Stop()
-                                      StimAWatch.Stop()
-                                      StimBWatch.Stop()
-                                      StimGrid.Background = Brushes.Black
-                                      'RecordData()
-                                  End If
-                              End Sub)
-        End Sub
+    'Private Sub BCh_StateChange(sender As Object, e As DigitalInputStateChangeEventArgs)
+    '    Dispatcher.Invoke(Sub()
+    '                          If e.State Then
+    '                              Latency.Stop()
+    '                              If (ActiveStimWatch.ElapsedMilliseconds <= 10000) Then
+    '                                  btnCount = btnCount + 1
+    '                                  ActiveStimWatch.Reset()
+    '                                  SetGridColor(btnCount)
+    '                              ElseIf (ActiveStimWatch.ElapsedMilliseconds >= 10000) Then
+    '                                  ActiveStimWatch.Reset()
+    '                                  StimAWatch.Reset()
+    '                                  StimBWatch.Reset()
+    '                              End If
+    '                          Else
+    '                              Latency.Start()
+    '                              ActiveStimWatch.Stop()
+    '                              StimAWatch.Stop()
+    '                              StimBWatch.Stop()
+    '                              StimGrid.Background = Brushes.Black
+    '                              'RecordData()
+    '                          End If
+    '                      End Sub)
+    'End Sub
 
-        Private Sub OnAttachHandler(sender As Object, e As AttachEventArgs)
-            Dispatcher.Invoke(Sub()
-                                  Log.WriteLine(LogLevel.Info, "Phidget button attached!")
-                              End Sub)
-        End Sub
+    Private Sub Virtual_BCh_StateChange(e As RoutedEventArgs) Handles VirtualBtn.Click
 
-        Private Sub LockOut()
-            bc.Close() 'prevent button activate 
-            System.Threading.Thread.Sleep(30000) '
-            bc.Open()
-        End Sub
-
-        ' Reset Stopwatches to 0 for new trial after pressTimer reaches 100 seconds
-        Private Sub ResetTrial()
-            StimGrid.Background = Brushes.Black
+        If e.Handled Then
             Latency.Stop()
+            If (ActiveStimWatch.ElapsedMilliseconds <= 10000) Then
+                btnCount = btnCount + 1
+                ActiveStimWatch.Reset()
+                SetGridColor(btnCount)
+            ElseIf (ActiveStimWatch.ElapsedMilliseconds >= 10000) Then
+                ActiveStimWatch.Reset()
+                StimAWatch.Reset()
+                StimBWatch.Reset()
+            End If
+        Else
+            Latency.Start()
             ActiveStimWatch.Stop()
             StimAWatch.Stop()
             StimBWatch.Stop()
+            StimGrid.Background = Brushes.Black
+            'RecordData()
+        End If
+
+    End Sub
+
+    Private Sub OnAttachHandler(sender As Object, e As AttachEventArgs)
+        Dispatcher.Invoke(Sub()
+                              Log.WriteLine(LogLevel.Info, "Phidget button attached!")
+                          End Sub)
+    End Sub
+
+    Private Sub LockOut()
+        bc.Close() 'prevent button activate 
+        System.Threading.Thread.Sleep(30000) '
+        bc.Open()
+    End Sub
+
+    ' Reset Stopwatches to 0 for new trial after pressTimer reaches 100 seconds
+    Private Sub ResetTrial()
+        StimGrid.Background = Brushes.Black
+        Latency.Stop()
+        ActiveStimWatch.Stop()
+        StimAWatch.Stop()
+        StimBWatch.Stop()
         'RecordData()
         PressWatch = 0
-            btnCount = 0
-            Latency.Reset()
-            ActiveStimWatch.Reset()
-            StimAWatch.Reset()
-            StimBWatch.Reset()
-        End Sub
+        btnCount = 0
+        Latency.Reset()
+        ActiveStimWatch.Reset()
+        StimAWatch.Reset()
+        StimBWatch.Reset()
+    End Sub
 
     'Private Sub EventListener()
 
@@ -176,14 +221,14 @@ Public Class ChimpWindow
 
     ' Clean up the Phidget resources when the application closes
     Protected Overrides Sub OnClosed(e As EventArgs)
-            If bc IsNot Nothing Then
-                bc.Close()
-            End If
-            'If fc IsNot Nothing Then
-            '    fc.Close()
-            'End If
-            MyBase.OnClosed(e)
-        End Sub
+        If bc IsNot Nothing Then
+            bc.Close()
+        End If
+        'If fc IsNot Nothing Then
+        '    fc.Close()
+        'End If
+        MyBase.OnClosed(e)
+    End Sub
 
 
 End Class
